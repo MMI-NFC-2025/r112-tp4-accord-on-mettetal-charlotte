a1d51f87bd74ad627ff6c99c3bf6ad2be7d9997c
mettetal-charlotte <charlotte.mettetal@edu.univ-fcomte.fr>
2025-11-28 16:48:21 +0100

    referme les détails suite à un clic sur un summary

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 // *** ACCORDEON ***
+const accordeons = document.querySelectorAll('.accordeon');
+const accordeon.querySelectorAll('details');
+accordeons.forEach(accordeon => {
+    const details = accordeon.querySelectorAll('details');
+    details.forEach(detail => { 
+        detail.addEventListener('toggle', () => {
+            if (detail.open) {
+                // Fermer les autres détails    
+                details.forEach(otherDetail => {
+                    if (otherDetail !== detail) {
+                        otherDetail.removeAttribute('open');
+                    }   
+                });
+            }
+        });
+    }
+    );
+}); 
